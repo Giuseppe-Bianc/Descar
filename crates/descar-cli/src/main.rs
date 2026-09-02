3747467e96f83621cd79a4daef18aad7b5ac5850
GIUSEPPE BIANCONI <75339264+Giuseppe-Bianc@users.noreply.github.com>
2026-09-02 14:13:45 +0200

    refactor: move CLI binary into cli crate

---
+++ crates/descar-cli/src/main.rs
@@ -0,0 +1,30 @@
+use clap::{CommandFactory, Parser};
+use descar_cli::cli::{Args, Command};
+
+fn main() {
+    let args = Args::parse();
+
+    match args.command {
+        None => Args::command().print_help().expect("failed to print help"),
+        Some(Command::Compile(args)) => {
+            if !args.logging.quiet {
+                match args.logging.verbose {
+                    0 => {}
+                    1 => println!("Compiling {}", args.input.display()),
+                    2 => println!("Compiling {} with debug diagnostics", args.input.display()),
+                    _ => println!("Compiling {} with trace diagnostics", args.input.display()),
+                }
+            }
+        }
+        Some(Command::Check(args)) => {
+            if !args.logging.quiet {
+                match args.logging.verbose {
+                    0 => {}
+                    1 => println!("Checking {}", args.input.display()),
+                    2 => println!("Checking {} with debug diagnostics", args.input.display()),
+                    _ => println!("Checking {} with trace diagnostics", args.input.display()),
+                }
+            }
+        }
+    }
+}
