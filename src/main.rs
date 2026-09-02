8fe8a6d569474826fa43e995b6598c8a07c5000f
GIUSEPPE BIANCONI <75339264+Giuseppe-Bianc@users.noreply.github.com>
2026-09-02 12:54:42 +0200

    refactor(cli): dispatch compile and check commands

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
 mod cli;
 
 use clap::Parser;
-use cli::Args;
+use cli::{Args, Command};
 
 fn main() {
     let args = Args::parse();
 
-    if args.verbose {
-        println!("Compiling {:?}", args.input.display());
+    match args.command {
+        Command::Compile(args) => {
+            if args.verbose {
+                println!("Compiling {}", args.input.display());
+            }
+        }
+        Command::Check(args) => {
+            if args.verbose {
+                println!("Checking {}", args.input.display());
+            }
+        }
     }
 }
