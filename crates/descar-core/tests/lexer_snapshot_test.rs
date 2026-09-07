3a5eb5f70b79270d707fa7788859cccf3d470064
GIUSEPPE BIANCONI <gbianconi3@gmail.com>
2026-09-07 16:50:30 +0200

    test(lexer): add insta snapshot test for lexer

---
+++ crates/descar-core/tests/lexer_snapshot_test.rs
@@ -0,0 +1,16 @@
+// Snapshot test for lexer using insta
+
+use descar_core::lex::lexer::Lexer;
+use descar_core::tokens::token_kind::TokenKind;
+
+#[test]
+fn lexer_snapshot() {
+    let input = "foo + 123 - bar * 45.6";
+    let mut lexer = Lexer::new("snapshot_test", input);
+    let mut token_kinds = Vec::new();
+    while let Some(tok) = lexer.next_token() {
+        token_kinds.push(tok.map(|t| t.kind));
+    }
+    // Debug snapshot of token kinds (including errors if any)
+    insta::assert_debug_snapshot!(token_kinds);
+}
